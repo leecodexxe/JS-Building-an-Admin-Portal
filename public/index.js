14b4bf95a3786799af1b1d8034effa5db7412696
leecodexxe~ <lee4works@gmail.com>
2023-05-21 12:48:12 -0700

    change

--- public/index.js
+++ public/index.js
@@ -1,28 +1,45 @@
 async function main() {
+  let response = await fetch("http://localhost:3001/listBooks");
+  let books = await response.json();
 
-    let response = await fetch('http://localhost:3001/listBooks')
-    let books = await response.json()
-
-    books.forEach(renderBook)
+  books.forEach(renderBook);
+  fetch("http://localhost:3001/updateBook", {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      id: 3,
+      title: "The Legends of Arathrae",
+      year: 2016,
+      description:
+        "This anthology tells the intertwined narratives of six fairy tales.",
+      quantity: 8,
+      imageURL: "/assets/arathrae.jpeg",
+    }),
+  });
 }
 
 function renderBook(book) {
-    let bookContainer = document.querySelector('.book-container')
-    bookContainer.innerHTML += `
+  let bookContainer = document.querySelector(".book-container");
+  bookContainer.innerHTML += `
         <div class="col-sm-3">
             <div class="card" style="width: 100%;">
-                ${book.imageURL ? `
+                ${
+                  book.imageURL
+                    ? `
                     <img class="card-img-top" src="${book.imageURL}" />
                 `
-                : ``}
+                    : ``
+                }
                 <div class="card-body">
                     <h5 class="card-title">${book.title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Available: ${
+                      book.quantity
+                    }</h6>
                     <p class="card-text">${book.description}</p>
                 </div>
             </div>
         </div>
-    `
+    `;
 }
 
-main()
+main();
